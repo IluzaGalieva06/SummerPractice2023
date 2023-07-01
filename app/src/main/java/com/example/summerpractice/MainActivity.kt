3adc65facd99b43fdd07286991ffec59a06a62a3
Galieva Iluza <galievailyu@yandex.ru>
2023-07-01 12:36:17 +0300

    hm1(1)

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -14,13 +14,14 @@ class MainActivity : AppCompatActivity() {
         val newBinging = ActivityMainBinding.inflate(layoutInflater)
         binding = newBinging
         setContentView(newBinging.root)
-
-        binding?.button?.setOnClickListener{
-            if(binding?.etName?.text?.toString()?.trim()?.equals("") == true || binding?.etName?.text!!.length > 50 || binding?.etHeight?.text?.toString()!!.toInt() > 250 || binding?.etHeight?.text?.toString()!!.toInt() < 0 || binding?.etWeight?.text?.toString()!!.toInt() > 250 || binding?.etWeight?.text?.toString()!!.toInt() < 0 || binding?.etAge?.text?.toString()!!.toInt() < 0 || binding?.etAge?.text?.toString()!!.toInt() > 150) {
-                binding?.textView3?.text = "The data is incorrect"
-            }else {
-                var cnt: Int = binding?.etWeight?.text?.toString()!!.toInt() * 10 + binding?.etHeight?.text?.toString()!!.toInt() * 6 - binding?.etAge?.text?.toString()!!.toInt() * 5 - binding?.etName?.toString()?.length!!.toInt()
-                binding?.textView3?.text = "Answer: $cnt"
+        binding?.run {
+            binding?.button?.setOnClickListener{
+                if(etName?.text.toString().trim().equals("") == true || etName?.text.toString().length > 50 || etHeight?.text.toString().toInt() > 250 || etHeight?.text.toString().toInt() < 0 || etWeight?.text.toString().toInt() > 250 || etWeight?.text.toString().toInt() < 0 || etAge?.text.toString().toInt() < 0 || etAge?.text.toString().toInt() > 150) {
+                    textView3?.text = "The data is incorrect"
+                }else {
+                    var cnt: Int = etWeight?.text.toString().toInt() * 10 + etHeight?.text.toString().toInt() * 6 - etAge?.text.toString().toInt() * 5 - etName?.text.toString().length.toInt()
+                    textView3?.text = "Answer: $cnt"
+                }
             }
         }
     }
